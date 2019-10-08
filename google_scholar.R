891aa470f2e7e5760b22557c0fa94d999878d112
Brian Stacy <bstacy@worldbank.org>
2019-10-08 16:35:28 -0400

    updates

--- google_scholar.R
+++ google_scholar.R
@@ -15,3 +15,29 @@ basic_info<-get_profile(id)
 #get my publication list
 pubs<-get_publications(id)
 
+#get citation history
+cites<-get_citation_history(id)
+
+ggplot(cites, aes(x=year, y=cites)) +
+  geom_bar(stat='identity', fill="steelblue") +
+  geom_text(aes(label=cites), vjust=1.6, color="white", size=3.5) +
+  theme_minimal() +
+  scale_y_continuous(name="Citations") +
+  ggtitle("Citations For All Articles")
+  
+
+# First let's get the data, filtering to only the items tagged as
+# Resume items
+position_data <- read_csv('positions.csv') %>% 
+  filter(in_resume) %>% 
+  mutate(
+    # Build some custom sections by collapsing others
+    section = case_when(
+      section %in% c('research_positions', 'industry_positions') ~ 'positions', 
+      section %in% c('data_science_writings', 'by_me_press') ~ 'writings',
+      TRUE ~ section
+    )
+  ) 
+
+position_data_merge <- position_data %>%
+  left_join(pubs)
